824e69ea464e934c0adc1a78e1ae5abdf7572de3
Ricky Bobby <richard.pajewski@gmail.com>
2020-12-16 08:56:42 -0600

    project structure added and connected to database/server

---
+++ controllers/api/plant-routes.js
@@ -0,0 +1,85 @@
+const router = require('express').Router()
+const { User, Plant, Room } = require('../../models')
+
+// get all plants
+router.get('/', (req, res) => {
+    Plant.findAll({
+
+    })
+    .then(dbPlantData => res.json(dbPlantData))
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+})
+
+// get one plant
+router.get('/:id', (req, res) => {
+    Plant.findOne({
+
+    })
+    .then(dbPlantData => {
+        if (!dbPlantData) {
+            res.status(404).json({ message: 'No plant found with this id!' })
+            return
+        }
+        res.json(dbPlantData)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+// create plant
+router.post('/', (req, res) => {
+    // expects {}
+    Plant.create({
+
+    })
+    .then(dbPlantData => res.json(dbPlantData))
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+})
+
+// update plant
+router.put('/:id', (req, res) => {
+    Plant.update({
+
+    })
+    .then(dbPlantData => {
+        if (!dbPlantData) {
+            res.status(404).json({ message: 'No plant found with this id!' })
+            return
+        }
+        res.json(dbPlantData)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+// delete plant
+router.delete('/:id', (req, res) => {
+    Plant.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPlantData => {
+        if (!dbPlantData) {
+            res.status(404).json({ message: 'No plant found with this id!' })
+            return
+        }
+        res.json(dbPlantData)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+module.exports = router
